aad3af8830e45bb6821dcecf9d7cabcc40b63fa1
ulilabzr <absoruu@gmail.com>
2026-07-06 05:48:16 +0700

    feat: add multi-stage Docker configuration for standalone deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Proxy ke Railway ditangani via src/app/api/v1/[...path]/route.ts
+  output: "standalone",
 };
 
 module.exports = nextConfig;
